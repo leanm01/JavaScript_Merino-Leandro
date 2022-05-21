ce2377037b2d80a7e263b72a4127ef2bc70a2394
leandro merino <60607751+leanm01@users.noreply.github.com>
2022-05-21 13:28:20 -0300

    act

---
+++ extension.js
@@ -0,0 +1,32 @@
+debugger
+
+let i;
+let ingreseNombre;
+let Edad;
+
+
+
+
+
+if(ingreseNombre =="" && Edad <=0)
+{
+    
+    alert("No se ha ingresado el nombre",ingreseNombre);
+    alert("No Se ha ingresado la edad",Edad);
+
+}else
+{
+
+    for(i=0;i<2;i++)
+    {
+    
+        let ingreseNombre = prompt("Ingrese su nombre: ");
+        let Edad = prompt("Ingrese su edad: ");
+    }
+
+    alert("se ha ingresado el nombre",ingreseNombre);
+    alert("Se ha ingresado la edad",Edad);
+    confirm("Bienvenido!!");
+
+}
+
