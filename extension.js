87a4df57bdf574291af71221aa517a9655bd0d77
leandro merino <60607751+leanm01@users.noreply.github.com>
2022-07-14 18:47:11 -0300

    act

--- extension.js
+++ extension.js
@@ -23,23 +23,9 @@ function AgregarUsuario()
     
     console.log(mostrarSiHayUS);
 
-    /*
-    if(newUser!=0 || newUser==1)
-    {
-
-         console.log("Se agrego un usuario");
-
-    }
-    else{
-
-         console.log("NO se agrego un usuario");
-
-    }*/
+   
 
 } 
-
-
-
 
 function MostrarUsuario()
 {
@@ -77,29 +63,7 @@ buttonCargarLs.addEventListener("click", function() {
 
 const usuarioDos= {...usuariosArray};
 
-/*
-function mailEnviado()
-{
 
-
-    let usuarioAux = usuariosArray.getIndex(parseInt(document.getElementById("inputDNI").value));
-
-    if(usuarioAux>=0)
-    {
-
-        emailjs.send("service_Leandro","template_om2ryyf",{
-            Lean: "hola",
-            reply_to: "recibio informacion",
-            });
-
-
-    }
-
-
-
-
-}
-*/
 
 //EMAILJS
 const btn = document.getElementById('button');
@@ -116,7 +80,7 @@ document.getElementById('form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Enviar Email';
-      alert('Enviado!');
+      alertify.success('Mail enviado');
     }, (err) => {
       btn.value = 'Enviar Email';
       alert(JSON.stringify(err));
@@ -124,35 +88,9 @@ document.getElementById('form')
 });
 
 
-/*
-function datosDeAlumno() 
-{
-     const datosAlumno = {nombre: inputNombre.value,
-                          telefono: inputTelefono.value,
-                          email: inputEmail.value,
-                          DNI: inputDNI.value};
-    
-    let str = JSON.stringify(datosAlumno);
-    
-    localStorage.setItem("datosAlumno", str);
-}
 
-function obtenerDatosAlumnos() 
-{
-    
-    if (localStorage.getItem("datosAlumno1")) {
-        const datosAlumno1 = JSON.parse(localStorage.getItem("datosAlumno1"))
-                inputNombre.value = datosAlumno1.nombre
-                inputTelefono.value = datosAlumno1.telefono
-                inputEmail.value  = datosAlumno1.email
-                inputDNI.value = datosAlumno1.DNI
-    }    
-    
-}
 
-obtenerDatosAlumnos(); 
 
-*/
 
 
 
@@ -175,110 +113,3 @@ obtenerDatosAlumnos();
 
 
 
-
-
-
-
-/*let promedio=0;
-let primerParametro;
-let segundoParametro;
-let  primerNota ;
-let  segundaNota ;
-*/
-/*
-
-
-
-//const ListadoDeAlumnosAprobados = document.querySelector("#ListadoDeAlumnosAprobados");
-
-//ALUMNOS HABILES
-
-function alumnosHabiles()
-{
-    ListadoDeAlumnos.innerHTML = "";
-    for(elemento of alumnos)
-    {
-       
-        const listAlum = document.createElement("li");
-        listAlum.id = "1" + elemento;
-        listAlum.innerText = elemento;
-        listAlum.className = "primer_ul .collection-item";
-
-        listAlum.addEventListener("click", ()=>{alumnosaprobados(`${listAlum.id}`)})
-
-        ListadoDeAlumnos.append(listAlum);
-       
-
-    }
-    console.log(ListadoDeAlumnos);
-}
-
-alumnosHabiles();
-
-
-function alumnosaprobados(alumn)
-{
-    const elemntoPlus = document.getElementById(alumn);
-
-    if(alumn !="")
-    {
-
-        const alumApr = document.createElement("li");
-        alumApr.className = "collection-item";
-        alumApr.innerText = elemntoPlus.innerText;
-        alumApr.id = `${alumn}aprobado`;
-
-
-        ListadoDeAlumnosAprobados.append(alumApr);
-
-    }
-
-
-
-}
-*/
-
-
-// CARGA DE NOTAS
-
-/*
-
-primerNota = parseInt(prompt("ingrese la primer nota: "));
-segundaNota = parseInt(prompt("ingrese la segunda nota: "));
-
-let resultado = calcularPromedioAlumno(primerNota,segundaNota);
-
-    if(resultado >=6)
-    {
-        alert("El promedio es: " + resultado + " - APROBADO");
-    }else{
-        alert("El promedio es: " + resultado + " - DESAPROBADO");
-    }
-
-
-function calcularPromedioAlumno(primerParametro, segundoParametro)
-{
-    return (primerParametro + segundoParametro) / 2 ;
-}
-
-*/
-
-
-//ARRAY
-
-/*
-const alumnosCargados=[{id:1, alumno:'Pepe Bolson'},
-                       {id:2, alumno:'Tobi Rosales'},
-                       {id:3, alumno:'Marco Rosalino'},
-                       {id:4, alumno:'Jose Leon'},
-                       {id:5, alumno:'Esteban Banquito'},
-                       {id:6, alumno:'Martina Roger'}];
-
-
-alert("Alumnos ya cargados");
-alumnosCargados.forEach(function(alumnosCargados)
-{
-
-    alert(alumnosCargados.id+" = "+alumnosCargados.alumno);
-
-});*/
